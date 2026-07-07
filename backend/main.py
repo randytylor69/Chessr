05816585ed560d801803f0cbe6fef90de5644a4c
ziyin <ziyinmao2006@gmail.com>
2026-07-06 21:22:40 -0400

    reverse back to web version; no CLI. Just a broken website.

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,6 @@ from dbFunctions import *
 from utilFunctions import *
 from server import *
 from dotenv import find_dotenv, load_dotenv
-import webbrowser
-import subprocess
 
 # load env variables from root
 dotenv_path = find_dotenv()
@@ -13,17 +11,10 @@ API_TOKEN = os.getenv("API_TOKEN")
 username = os.getenv("username")
 
 def main():
-    # Calls FE to open localhost.
-    # PROBLEM: code stops executing after subprocess.run
-    cmd = "cd ../frontend ; npm run dev"
-    subprocess.run(cmd, shell=True)
-    # CODE STOPS EXECUTING
-    webbrowser.open_new_tab("http://localhost:5173/")
     deleteAllGamesFromDB()
     addAllGamesToDB(username, API_TOKEN)
     condenseGamesToDB()
-    app.run()
-
+    app.run(debug=True)
 
 if __name__ == "__main__":
     main()
